2ded0323923aecf6a86be59bf82488e485323728
Guillaume Verbiguié <39549008+GuillaumeVerb@users.noreply.github.com>
2024-10-11 13:23:11 +0200

    Update my_second_model.sql

--- models/example/my_second_model.sql
+++ models/example/my_second_model.sql
@@ -1,4 +1,9 @@
 
-
 -- Use the `ref` function to select from other models
-select * from {{ ref('my_first_dbt_model') }}
+
+select *
+from {{ ref('my_first_dbt_model') }}
+--from  {{source ('PUBLIC', 'DEMOGRAPHICS' ) }}
+where TOTAL_POPULATION > 1000000
+
+
